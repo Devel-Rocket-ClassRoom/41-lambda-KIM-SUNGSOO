8b7c462cec09ec9a21fed0b2c80a96ee8086a227
KIM-SUNGSOO <133596829+KIM-SUNGSOO@users.noreply.github.com>
2026-03-10 14:26:00 +0900

    CodPRac

--- CodingPractice-01/Program.cs
+++ CodingPractice-01/Program.cs
@@ -1,4 +1,64 @@
 using System;
+using System.Runtime.InteropServices;
+using System.Xml;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+class CodPrac
+{
+    static void Main()
+    {
+        //Prac_1();
+        //Prac_2();
+        //Prac_3();
+        //Prac_4();
+        Prac_5();
+    }
+
+    static void Prac_1()
+    {
+        PrintMessage hello = () => Console.WriteLine("안녕하세요");
+
+        hello();
+
+    }
+
+    static void Prac_2()
+    {
+        Transformer square = x => x * x;
+
+        Console.WriteLine(square(3));
+        Console.WriteLine(square(4));
+        
+    }
+
+    static void Prac_3()
+    {
+        IsLonger isLong = (string msg, int len) => msg.Length > len;
+        Console.WriteLine(isLong("안녕하세요.", 5));
+        Console.WriteLine(isLong("반갑습니다.", 10));
+    }
+
+    static void Prac_4()
+    {
+        Greeting greet = () =>
+        {
+            Console.WriteLine("안녕하세요.");
+            Console.WriteLine("반갑습니다.");
+        };
+        greet();
+    }
+
+    static void Prac_5()
+    {
+        Calculator add = (a, b) =>
+        {
+            int result = a + b;
+            Console.WriteLine($"{a} + {b} = {result}");
+            return result;
+        };
+
+        int sum = add(3, 5);
+        Console.WriteLine($"합계: {sum}");
+    }
+}
+
---
+++ CodingPractice-01/delegate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+delegate void PrintMessage();
+
+delegate int Transformer(int i);
+
+delegate bool IsLonger(string msg, int len);
+
+delegate void Greeting();
+
+delegate int Calculator(int a, int b);
